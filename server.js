3905b6465873a02b77e05665ad3c7b4853a1499e
kreid333 <kai.j.reid@gmail.com>
2020-11-04 20:19:22 -0500

    Added build folder static middleware and route to server.js

--- server.js
+++ server.js
@@ -12,25 +12,32 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(express.static("client/build"));
+
 // ESTABLISHING MONGOOSE CONNECTION
 mongoose.connect("mongodb://localhost/googleBooks", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // LOGGING CONNECTION TO CONSOLE
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-    console.log("Mongoose successfully connected!");
-})
+  console.log("Mongoose successfully connected!");
+});
 
 connection.on("error", (err) => {
-    console.log("Mongoose connection error: ", err);
-})
+  console.log("Mongoose connection error: ", err);
+});
 
 // LISTENING ON PORT
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
-})
+  console.log(`App listening at http://localhost:${PORT}`);
+});
+
+// PRODUCTION
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
